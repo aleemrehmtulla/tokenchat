d9eaa9cca81dd20a3314e1e2feb25ffe310cde01
Aleem Rehmtulla <60443878+aleemrehmtulla@users.noreply.github.com>
2022-02-13 22:32:17 -0700

    Add $LFG

--- pages/components/header.js
+++ pages/components/header.js
@@ -49,12 +49,12 @@ function Header() {
       <div className="md:flex hidden p-8  justify-center ">
       <div className="flex space-x-12 text-3xl items-center">
        
-          <a href="?DAI" className="link link-underline link-underline-black text-black">
-            $DAI
+          <a href="?LFG" className="link link-underline link-underline-black text-black">
+            $LFG
           </a>
  
-          <a href="?VITA" className="link link-underline link-underline-black text-black">
-            $VITA
+          <a href="?DAI" className="link link-underline link-underline-black text-black">
+            $DAI
           
           </a>
           <a href="?BAE" className="link link-underline link-underline-black text-black" target={"_self"}>
